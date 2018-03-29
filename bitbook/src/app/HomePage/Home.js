f1ae336bd76677f081cf38abded7f6ce912f8108
BIT Student <classroom@bgit.rs>
2018-03-29 13:06:07 +0200

    feature complete

--- bitbook/src/app/HomePage/Home.js
+++ bitbook/src/app/HomePage/Home.js
@@ -14,46 +14,90 @@ class Home extends Component {
             videoPosts: [],
             imagePosts: [],
             modalIsOpen: false,
-            input: " ",
-            selectedPosts: []
-
+            input: "",
+            selectedPosts: [],
+            message: ''
         }
     }
 
-  
 
-    componentDidMount() {
+    getAllPosts = () => {
         dataServices.getPosts()
-            .then((myPosts) => {
+            .then(myPosts => {
                 this.setState({
                     textPosts: myPosts.textPosts,
                     videoPosts: myPosts.videoPosts,
                     imagePosts: myPosts.imagePosts,
-                    selectedPosts:  myPosts.textPosts.concat(myPosts.videoPosts, myPosts.imagePosts)
+                    selectedPosts: myPosts.textPosts.concat(myPosts.videoPosts, myPosts.imagePosts)
                 })
             });
     }
 
+    componentDidMount() {
+        this.getAllPosts()
+    }
 
-    handleInputChange = event => {
-        this.setState ({
+    handleInputChange = (event) => {
+        this.setState({
             input: event.target.value
         })
     }
 
-    checkTextInput= () => {
-        if(this.state.input.length > 5 ){
-            return(
-                console.log('Text is too long')
-            )
+    checkTextInput = () => {
+        if (this.state.input.length > 1000) {
+            this.setState({
+                message: 'Text is too long'
+            });
         } else {
-            dataServices.addNewTextPost({newTextPost : this.state.input})
+            dataServices.addNewTextPost(this.state.input)
+                .then((response) => {
+                    this.getAllPosts();
+                    this.closeModal();
+                }
+                )
         }
     }
 
-    
+    checkImageInput = () => {
+        const myUrlLength = this.state.input.length;
+        const cutUrl = this.state.input.slice(myUrlLength -3);
+        const cutUrlJpeg = this.state.input.slice(myUrlLength -4);
 
+        if (cutUrl != 'jpg' && cutUrl != 'png' && cutUrl != 'gif' && cutUrlJpeg != 'jpeg') {
+            this.setState({
+                message: 'Post is not image'
+            });
+        } else {
+            dataServices.addNewImagePost(this.state.input)
+                .then((response) => {
+                    this.getAllPosts();
+                    this.closeModal();
+                }
+                )
+        }
+    }
 
+    createLink = (link) =>{
+        return link.replace("watch?v=", "embed/");
+    }
+
+    checkVideoInput = () => {
+
+        if (this.state.input.includes("embed/")) {
+            this.setState({
+                message: 'Post is not video'
+            });
+        } else {
+            let data = this.createLink(this.state.input);
+            console.log(data)
+            dataServices.addNewVideoPost(data)
+                .then((response) => {
+                    this.getAllPosts();
+                    this.closeModal();
+                }
+                )
+        }
+    }
 
     sortPosts = () => {
         let allPosts = this.state.textPosts.concat(this.state.videoPosts, this.state.imagePosts)
@@ -65,21 +109,22 @@ class Home extends Component {
             return 0;
         })
         return sortPosts;
+    }
 
     handleChange = (event, data) => {
-     if(data.value === "text"){
-         this.setState({
-             selectedPosts : this.state.textPosts
-         })
-     } else if(data.value === "images"){
-         this.setState({
-             selectedPosts : this.state.imagePosts
-         })
-     } else {
-         this.setState({
-             selectedPosts : this.state.videoPosts
-         })
-     }
+        if (data.value === "text") {
+            this.setState({
+                selectedPosts: this.state.textPosts
+            })
+        } else if (data.value === "images") {
+            this.setState({
+                selectedPosts: this.state.imagePosts
+            })
+        } else {
+            this.setState({
+                selectedPosts: this.state.videoPosts
+            })
+        }
 
     }
 
@@ -95,69 +140,69 @@ class Home extends Component {
     }
 
     closeModal = () => {
-        this.setState({ modalIsOpen: false });
+        this.setState({
+            modalIsOpen: false,
+            input : ''
+        });
     }
 
-    renderTextModal =() => {
-            return (
-                <div>
-                    <h2 ref={subtitle => this.subtitle = subtitle}>New text post</h2>
-                    <button onClick={this.closeModal}>close</button>
-                    <div>Text content</div>
-                    <form>
-                        <input type='text' value={this.state.input} onChange={this.handleInputChange}/>
-                        <button onClick={this.checkTextInput()}>POST</button>
-                    </form>
-                </div>
-            )
-        
+    renderTextModal = () => {
+        return (
+            <div>
+                <h2 ref={subtitle => this.subtitle = subtitle}>New text post</h2>
+                <button onClick={this.closeModal}>close</button>
+                <div>Text content</div>
+                <form>
+                    <input type='text' value={this.state.input} onChange={this.handleInputChange} />
+                    <button onClick={this.checkTextInput}>POST</button>
+                </form>
+                <div>{this.state.message}</div>
+            </div>
+        )
+
     }
 
-
-
-
-    renderImageModal =() =>{
-
-
+    renderImageModal = () => {
+        console.log(this.state.input)
         return (
             <div>
                 <h2 ref={subtitle => this.subtitle = subtitle}>New image post</h2>
                 <button onClick={this.closeModal}>close</button>
                 <div>Image source</div>
                 <form>
-                    <input type ='url'/>
-
-                    <button>POST</button>
+                    <input type='url' onChange={this.handleInputChange} />
+                    <button onClick={this.checkImageInput}>POST</button>
                 </form>
+                <div>{this.state.message}</div>
             </div>
         )
     }
 
 
-    renderVideoModal =()=> {
+    renderVideoModal = () => {
         return (
             <div>
                 <h2 ref={subtitle => this.subtitle = subtitle}>New video post</h2>
                 <button onClick={this.closeModal}>close</button>
                 <div>YouTube video link</div>
                 <form>
-                    <input type='url'/>
-                    <button>POST</button>
+                    <input type='url' onChange={this.handleInputChange}/>
+                    <button onClick={this.checkVideoInput}>POST</button>
                 </form>
+                <div>{this.state.message}</div>
             </div>
         )
     }
 
-    renderModalComponent() {
+    renderModalComponent = () => {
         switch (this.state.currentModal) {
-            case "text" : return this.renderTextModal()
+            case "text": return this.renderTextModal()
             case "image": return this.renderImageModal()
-            case "video" : return this.renderVideoModal()
+            case "video": return this.renderVideoModal()
         }
     }
 
     render() {
-        console.log(this.state.input)
         return (
             <div className="ui three column grid">
                 <div className="row">
@@ -166,11 +211,11 @@ class Home extends Component {
                     </div>
                     <div className='four wide column'>
 
- 
+
                         <NewPostButton openPost={this.openModal} />
 
-                        <MenuAllPosts handleChange={this.handleChange}/>
-               
+                        <MenuAllPosts handleChange={this.handleChange} />
+
 
                     </div>
                 </div>
