d1896b13df174bdd95650b6bfa72d3157f4b6ec6
BIT Student <classroom@bgit.rs>
2018-03-30 09:58:04 +0200

    header changes

--- bitbook/src/app/partials/Header.js
+++ bitbook/src/app/partials/Header.js
@@ -4,18 +4,12 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
 
-        <div className="ui grid bgColor">
+    <div className="ui grid bgColor">
         <div className="two wide column"></div>
             <div className="two wide column">
                 <h1 className='logo-app'>Bitbook</h1>
-
-        <div className="ui grid header">
-            <div className="eight column row">
-                <div className="column"></div>
-                <div className="column">
-                    <h1>Bitbook</h1>
-
                 </div>
+
             <div className="eight wide column"></div>
                 <div className="one wide column nav-links">
                     <Link to="/">
@@ -29,11 +23,7 @@ const Header = () => {
                     </Link>
 
                 </div>
-
                 <div className="one wide column nav-links">
-                    <a className="item">
-
-                <div className="column">
                     <Link to="/profile">
 
                         Profile
