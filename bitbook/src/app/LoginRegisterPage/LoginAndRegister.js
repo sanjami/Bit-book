50a522a3b0a5439d7f0f67dac84a46914266f5c9
BIT Student <classroom@bgit.rs>
2018-04-05 17:22:49 +0200

    Style login

--- bitbook/src/app/LoginRegisterPage/LoginAndRegister.js
+++ bitbook/src/app/LoginRegisterPage/LoginAndRegister.js
@@ -3,6 +3,7 @@ import { Tab, Container, Grid, Segment, Form, Button } from 'semantic-ui-react';
 import { authService } from '../../service/authenticationService';
 import ErrorComponeny from '../sharedComponents/ErrorComponent';
 import ErrorComponent from '../sharedComponents/ErrorComponent';
+import GridColumn from 'semantic-ui-react/dist/commonjs/collections/Grid/GridColumn';
 
 
 
@@ -233,23 +234,34 @@ class LoginAndRegister extends React.Component {
     render() {
 
         return (
-            <Container className='welcome-container'>
-                {/* <Grid.Row>
+            <React.Fragment>
+                <Grid id='main' stackable columns={4} >
+                    <Grid.Column  width={1} ></Grid.Column>
+                    <Grid.Column  computer={7} tablet={14}  className='welcome-container'>
+                        {/* <Grid.Row>
                     <Grid.Column width={8} className='headline-app'> */}
-                        <Segment id='login'>
-                            <h1>Welcome to Bitbook</h1><br/>
+                        <div id='login'>
+                            <h1>Welcome to Bitbook</h1><br />
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                        </Segment>
+                        </div>
+
+                    </Grid.Column>
                     {/* </Grid.Column> */}
                     {/* <Grid.Column width={8} className='tabs'> */}
-                        <Segment id='tabs'>
-                            <Tab panes={[{ menuItem: 'Login', render: () => <Tab.Pane attached={false}>{this.login()} </Tab.Pane> },
+                    {/* <Segment> */}
+                    <Grid.Column  computer={5} tablet={14} className='tabs' >
+                        <div id='tabs'>
+                            <Tab panes={[{ menuItem: 'Login', render: () => <Tab.Pane attached={false}>{this.login()}  </Tab.Pane> },
                             { menuItem: 'Register', render: () => <Tab.Pane attached={false}>{this.register()}</Tab.Pane> }]} menu={{ pointing: true }} />
-                            <ErrorComponent errorMessage={this.state.errorLogBtn}/>
-                        </Segment>
-                    {/* </Grid.Column> */}
-                {/* // </Grid.Row> */}
-            </Container>
+                            <ErrorComponent errorMessage={this.state.errorLogBtn} />
+                        </div>
+                        {/* </Grid.Column> */}
+                        {/* // </Grid.Row> */}
+                    </Grid.Column>
+                    {/* </Segment> */}
+                    <Grid.Column  width={1} ></Grid.Column>
+                </Grid>
+            </React.Fragment>
         )
     }
 
